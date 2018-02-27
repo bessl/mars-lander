2d3c25772af9fbb109b2c6057943b6bbe9abd8e0
Bernhard Essl <bernhardessl@aeoontechnologies.com>
2018-02-27 20:28:46 +0100

    include font

--- MarsLander/mainwindow.cpp
+++ MarsLander/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFontDatabase>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QFontDatabase::addApplicationFont(":/res/Px437_IBM_ISO9.ttf");
+    QFont font("PX437 IBM ISO9", 11, 1);
 }
 
 MainWindow::~MainWindow()
